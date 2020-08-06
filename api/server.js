1432fa3092e3d7163645aa7f5cfa8ae70d91df8c
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-08-06 12:53:13 -0400

    Update

--- api/server.js
+++ api/server.js
@@ -9,4 +9,8 @@ server.use(helmet());
 
 server.use('/api', apiRouter);
 
+server.get('/', (req, res) => {
+    res.status(200).json({ Hello: "Full stack Web developer"})
+})
+
 module.exports = server;
